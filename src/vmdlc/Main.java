bd20846a6eece85f619c742aaf2691da6b3f8cf4
Yuta Hiraswa <h1610756@edu.cc.uec.ac.jp>
2019-10-25 14:32:02 +0900

    警告（未使用変数など）を修正

--- src/vmdlc/Main.java
+++ src/vmdlc/Main.java
@@ -8,7 +8,6 @@
  */
 package vmdlc;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
